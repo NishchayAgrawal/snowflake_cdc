fcafec38b66768f3b39c9e8d86ab57590c6e8bd3
shreyasms <shreyas.ms99@gmail.com>
2023-07-26 08:28:59 +0530

    Update MySQL - Create_Table.sql

--- MySQL - Create_Table.sql
+++ MySQL - Create_Table.sql
@@ -11,12 +11,3 @@ CREATE or replace TABLE regions
     region_id    NUMBER
   );
  
-CREATE or REPLACE TABLE locations
-  (
-    location_id NUMBER PRIMARY KEY       ,
-    address     VARCHAR2( 255 ) NOT NULL,
-    postal_code VARCHAR2( 20 )          ,
-    city        VARCHAR2( 50 )          ,
-    state       VARCHAR2( 50 )          ,
-    country_id  CHAR( 2 )
-  );
